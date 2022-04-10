c7c7a42461241b732ffc7a09d44bde0af768b7d4
Sahar Mohsni <sahar.mohsni1990@gmail.com>
2022-04-10 23:09:56 +0100

    add css

--- src/components/filtredCocktailListSection/FiltredCocktailListSection.jsx
+++ src/components/filtredCocktailListSection/FiltredCocktailListSection.jsx
@@ -3,20 +3,21 @@ import { TitleContiner } from "../styles/cocktailListSection/TitleContiner.style
 import { SectionBackgroundColor } from "../styles/SectionBackgroundColor.styled";
 import { SectionContainer } from "../styles/SectionContainer.styled";
 import CocktailCard from "./cocktailCard/CocktailCard";
-
+import "./filtredCocktailListSection.scss";
 const FiltredCocktailListSection = ({
   filtredCocktailList,
   filtredCocktailListRef,
 }) => {
-
   return (
     <div id="filtred-cocktail-list-section" ref={filtredCocktailListRef}>
       <SectionBackgroundColor color="226, 229, 231 ">
         <SectionContainer>
           <TitleContiner>Cocktail List combination</TitleContiner>
-          {filtredCocktailList.map((cocktail) => (
-            <CocktailCard cocktail={cocktail} />
-          ))}
+          <div className="filtred-cocktail-list-section__list">
+            {filtredCocktailList.map((cocktail) => (
+              <CocktailCard cocktail={cocktail} />
+            ))}
+          </div>
         </SectionContainer>
       </SectionBackgroundColor>
     </div>
